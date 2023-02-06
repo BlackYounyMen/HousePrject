5c5df522b0bdf50d91c596dee84cd4d99cb558bd
yaxuan.guo <2980142742@qq.com>
2023-02-06 21:16:52 +0800

    gyx commit

--- src/views/Contract/ContractInfo/ContractAdd.vue
+++ src/views/Contract/ContractInfo/ContractAdd.vue
@@ -500,13 +500,12 @@ export default {
       this.$refs.Subscript.validate((valid) => {
         if (valid) {
           if (localStorage.getItem("Contract") == null) {
-            console.log(this.Subscript);
             var list = [];
             list.push(this.Subscript);
             localStorage.setItem("Contract", JSON.stringify(list));
           } else {
             var d = JSON.parse(localStorage.getItem("Contract"));
-            console.log(this.Subscript);
+
             d.push(this.Subscript);
             localStorage.setItem("Contract", JSON.stringify(d));
           }
@@ -535,7 +534,6 @@ export default {
     //保存数据
     AddCustomer() {
       this.CustomerAdd(this.contractInfo);
-
       console.log(this.BackState);
       if (this.BackState == true) {
         this.$message({
@@ -556,6 +554,7 @@ export default {
     const hour = date.getHours(); //获取当前小时数(0-23)
     const minute = date.getMinutes(); //获取当前分钟数(0-59)
     const second = date.getSeconds(); //获取当前秒数(0-59)
+
     var d = `${name}${year}${month}${day}${hour}${minute}${second}`;
     this.contractInfo.contractId = d;
     this.getCustomer();
@@ -563,3 +562,6 @@ export default {
   },
   store,
 };
+</script>
+<style>
+</style>
